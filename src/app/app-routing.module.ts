190f17d5fc750211a3eb60ea3898ed18505341c3
MicuAlexandruVlad <micualexandru131@yahoo.com>
2020-08-07 16:42:57 +0300

    2nd commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+const routes: Routes = [{
+  path: 'landing',
+  component: LandingComponent
+}, {
+  path: 'login',
+  component: LoginComponent
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
